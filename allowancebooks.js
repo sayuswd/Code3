ffea0d23b8e5e22370b3830ed8caea0aacf50304
sayuswd <sisi1013hola@yahoo.co.jp>
2019-04-18 00:46:00 +0900

    値の取得部分の短縮

--- allowancebooks.js
+++ allowancebooks.js
@@ -1,158 +1,112 @@
+//値を抜き出して計算する関数
+function get_input_value(name)
+{
+  //値を０に設定する。
+  let value = 0;
+  //医療費のinqutを取得
+  const input = document.querySelector(name);
+  //数値に変換
+  value = parseInt(input.value);
+  return value;
+}
+
 function textTipeValue() {
-
-  let all_input_value = 0
-  //全ての値を取得
-  const all_input_name = document.querySelectorAll(".allowancebooks_01 input");
-  //1つずつ値を取得
-  all_input_name.forEach(function(price){
-  all_input_value = parseInt(price.value);
-  alert(all_input_value);
-  });
-
 
 
   //food_expensesの値を取得
-  //値を０に設定する。
-  let food_input_value = 0;
-  //食費のinqutを取得
-  const food_input = document.querySelector(".food_expenses input");
-  //数値に変換
-  food_input_value = parseInt(food_input.value);
+  let food_input_value = get_input_value(".food_expenses input");
 
 
-
-  //medical_expensesの値を取得
-  //値を０に設定する。
-  let medical_input_value = 0;
-  //医療費のinqutを取得
-  const medical_input = document.querySelector(".medical_expenses input");
-  //数値に変換
-  medical_input_value = parseInt(medical_input.value);
-
+//medical_expensesの値を取得
+  let medical_input_value = get_input_value(".medical_expenses input");
 
 
   //library_expensesの値を取得
-  //値を０に設定する。
-  let library_input_value = 0;
-  //図書費のinqutを取得
-  const library_input = document.querySelector(".library_expenses input");
-  //数値に変換
-  library_input_value = parseInt(library_input.value);
-
+  let library_input_value = get_input_value(".library_expenses input");
 
 
   //entertainment_expenses_oの値を取得
-  //値を０に設定する。
-  let entertainment_o_input_value = 0;
-  //交際費（対外）のinqutを取得
-  const entertainment_o_input = document.querySelector(".entertainment_expenses_o input");
-  //数値に変換
-  entertainment_o_input_value = parseInt(entertainment_o_input.value);
-
+  let entertainment_o_input_value = get_input_value(".entertainment_expenses_o input");
 
 
   //entertainment_expenses_pの値を取得
-  //値を０に設定する。
-  let entertainment_p_input_value = 0;
-  //交際費（私的）のinqutを取得
-  const entertainment_p_input = document.querySelector(".entertainment_expenses_p input");
-  //数値に変換
-  entertainment_p_input_value = parseInt(entertainment_p_input.value);
-
+  let entertainment_p_input_value = get_input_value(".entertainment_expenses_p input");
 
 
   //traveling_expensesの値を取得
-  //値を０に設定する。
-  let traveling_input_value = 0;
-  //旅費交通費のinqutを取得
-  const traveling_input = document.querySelector(".traveling_expenses input");
-  //数値に変換
-  traveling_input_value = parseInt(traveling_input.value);
-
+  let traveling_input_value = get_input_value(".traveling_expenses input");
 
 
   //utilities_expensesの値を取得
-  //値を０に設定する。
-  let utilities_input_value = 0;
-  //水道光熱費のinqutを取得
-  const utilities_input = document.querySelector(".utilities_expenses input");
-  //数値に変換
-  utilities_input_value = parseInt(utilities_input.value);
-
+  let utilities_input_value = get_input_value(".utilities_expenses input");
 
 
   //miscellaneous_expensesの値を取得
-  //値を０に設定する。
-  let miscellaneous_input_value = 0;
-  //雑費のinqutを取得
-  const miscellaneous_input = document.querySelector(".miscellaneous_expenses input");
-  //数値に変換
-  miscellaneous_input_value = parseInt(miscellaneous_input.value);
-
+  let miscellaneous_input_value = get_input_value(".miscellaneous_expenses input");
 
 
   //giftの値を取得
-  //値を０に設定する。
-  let gift_input_value = 0;
-  //贈答費のinqutを取得
-  const gift_input = document.querySelector(".gift input");
-  //数値に変換
-  gift_input_value = parseInt(gift_input.value);
-
+  let gift_input_value = get_input_value(".gift input");
 
 
   //rewardの値を取得
-  //値を０に設定する。
-  let reward_input_value = 0;
-  //ご褒美のinqutを取得
-  const reward_input = document.querySelector(".reward input");
-  //数値に変換
-  reward_input_value = parseInt(reward_input.value);
-
+  let reward_input_value = get_input_value(".reward input");
 
 
   //連想配列で値を纏める
-  let expenses_list = [{
+  const expenses_list = [{
       name: "食費",
+      id: "food",
       price: food_input_value
     },
     {
       name: "医療費",
+      id: "medical",
       price: medical_input_value
     },
     {
       name: "図書費",
+      id: "library",
       price: library_input_value
     },
     {
       name: "交際費（対外）",
+      id: "entertainment_o",
       price: entertainment_o_input_value
     },
     {
       name: "交際費（私的）",
+      id: "entertainment_p",
       price: entertainment_p_input_value
     },
     {
       name: "旅費交通費",
+      id: "traveling",
       price: traveling_input_value
     },
     {
       name: "水道光熱費",
+      id: "utilities_expenses",
       price: utilities_input_value
     },
     {
       name: "雑費",
+      id: "miscellaneous_expenses",
       price: miscellaneous_input_value
     },
     {
       name: "贈答費",
+      id: "gift",
       price: gift_input_value
     },
     {
       name: "ご褒美",
+      id: "reward",
       price: reward_input_value
     },
   ];
+
+
 
   var sum = 0;
   const expensesElement = document.querySelector('.expenses p');
